94b74b75f33afe7ed5c9f8e7931965458ba5a7bb
eswar <mullapudieswar20@gmail.com>
2020-10-07 15:32:50 +0530

    imprted toastr and fortawesome in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconnyComponent } from './iconny/iconny.component';
 
+//fortawesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//ngx-toastr
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    IconnyComponent
-  ],
+  declarations: [AppComponent, IconnyComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
