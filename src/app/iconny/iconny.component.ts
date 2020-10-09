4b915305b291e41db33b8d6bbba58109b50f721f
eswar <mullapudieswar20@gmail.com>
2020-10-09 18:41:29 +0530

    added fa-icons in iconny component

--- src/app/iconny/iconny.component.ts
+++ src/app/iconny/iconny.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-iconny',
   templateUrl: './iconny.component.html',
-  styleUrls: ['./iconny.component.css']
+  styleUrls: ['./iconny.component.css'],
 })
 export class IconnyComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() iconName: string;
+  faPen = faPen;
+  faTimes = faTimes;
+  faCircle = faCircle;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
